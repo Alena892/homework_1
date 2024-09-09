a1bc5b2ac2edced29cf6c11461f62f413a51341a
User <User@DESKTOP-EESJB7M>
2024-09-09 22:45:30 +0300

    Добавьте файлы проекта.

---
+++ homework_1/Source.c
@@ -0,0 +1,12 @@
+#include <locale.h>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	puts("     _________   ");
+	puts("    /         \\");
+	puts(" __/           \\_____");
+	puts("|  ____       ____    |");
+	puts("|_|    |_____|    |___|");
+	puts("  |____|     |____|");
+}
